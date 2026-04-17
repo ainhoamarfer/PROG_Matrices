db72c9358cc239e5a3b461f9f22f035673a9453e
ainhoamarfer <ainhoa3mf@gmail.com>
2026-04-17 18:57:27 +0200

    Ejercicio Matriz

---
+++ src/main/java/com/ainhoamarfer/App.java
@@ -0,0 +1,19 @@
+package com.ainhoamarfer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+
+    public static void rellenarMatriz(double[][] matriz) {
+
+        for (int i = 0; i < matriz.length; i++) {
+            var fila = matriz[i];
+            for (int j = 0; j < matriz.length; j++) {
+                var casilla = matriz[j][i];
+            }
+        }
+    }
+}
